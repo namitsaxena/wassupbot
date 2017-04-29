32560b95418769054c61640aa6cac8000507bd17
namit <namit@namitsaxena.com>
2017-04-29 12:48:42 -0500

    run-image.sh updated so that it gives a clean error message without correct credentials instead of stack trace

--- run-image.sh
+++ run-image.sh
@@ -1,9 +1,11 @@
+# The image will try to run the bot (will ERROR without proper configuration)
+
 # This will run interactively. Press CTRL+C to kill
 # the configuration file should contain all relevant information
-# docker run wassupbot 
+#docker run wassupbot 
 
-# To run in background / detached mode (-d)
+# To run in background / detached mode add (-d)
 # to kill, run docker ps and then docker stop containerid
 export WA_PASSWORD='YOUR PASSWORD'
 export CHAT_ENGINE_KEY='YOUR KEY'
-docker run -d -e WA_PHONE='YOUR NUMBER' -e WA_PASSWORD -e CHAT_ENGINE_KEY wassupbot 
+docker run -e WA_PHONE='000' -e WA_PASSWORD -e CHAT_ENGINE_KEY wassupbot 
